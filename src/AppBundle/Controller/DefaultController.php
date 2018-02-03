9bf8087dade694803eaa2e2afe16d157b29abd80
Anton Romanov <anton.v.romanov@gmail.com>
2018-02-04 01:48:02 +0800

    frontend pages to show categories and their products

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Pagination\PaginationFactory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,13 +11,33 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="categories")
      */
-    public function indexAction(Request $request)
+    public function categoriesAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $em = $this->get('doctrine')->getManager();
+        $dql = $em->getRepository('AppBundle:Category')->dqlAll();
+        $pager = $this->get(PaginationFactory::class)->create($dql, $request);
+
+        return $this->render('AppBundle:Category:list.html.twig', [
+            'pagination' => $pager
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="category_products")
+     *
+     * @ParamConverter("category", class="AppBundle:Category", options={"mapping": {"slug": "slug"}})
+     */
+    public function categoryProductAction(Request $request, $category)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $dql = $em->getRepository('AppBundle:Product')->dqlByCategory($category);
+        $pager = $this->get(PaginationFactory::class)->create($dql, $request);
+
+        return $this->render('AppBundle:Category:show.html.twig', [
+            'object' => $category,
+            'pagination' => $pager
         ]);
     }
 }
